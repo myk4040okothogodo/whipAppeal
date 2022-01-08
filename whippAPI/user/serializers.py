63259a92a8c33ce426e961ab259656be8dd9a277
myk4040okothogodo <65109328+myk4040okothogodo@users.noreply.github.com>
2022-01-08 18:20:38 +0300

    user update

--- whippAPI/user/serializers.py
+++ whippAPI/user/serializers.py
@@ -7,3 +7,27 @@ class UserSerializer(serializers.ModelSerializer):
     model = User
     fields = ['id', 'username','email', 'is_active', 'date_joined']
     read_only_field = ['is_active', 'date_joined']
+  
+  def update(self, instance, validated_data):
+    """ Performs an update on a User."""
+    """
+    passwords shouldnt be handled with 'setattr', unlike other fields,Django provides
+    a function that handles hashing and salting of passwords.That means we need to remove
+    the password field from validated data before iterating over it.
+    """
+    password = validated_data.pop('password', None)
+    for (key, value) in validated_data.items():
+      #for the keys remaining in 'validated_data', we will set them on the current current
+      #'User' instance one at a time.
+      setattr(instance, key, value)
+      
+      if password is not None:
+        # '.set_password()' handles all
+        # of the security stuff we shouldnt be concerned with
+        instance.set_password(password)
+        #After everything has been updated we must explicitly save the model
+        #'.set_password()' deosnt save the model
+        instance.save()
+        return instance
+    
+  
