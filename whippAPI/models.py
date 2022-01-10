e6dfbcc623b87c288afc02e93b93633aa8561825
myk4040okothogodo <65109328+myk4040okothogodo@users.noreply.github.com>
2022-01-10 17:24:01 +0300

    Update models.py

--- whippAPI/models.py
+++ whippAPI/models.py
@@ -3,15 +3,16 @@ from django.conf import settings
 # Create your models here.
 import moneyed
 from djmoney.models.fields import MoneyField
+import datetime
 
 
 class Account(models.Model):
   owner = models.ForeignKey( settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
   renewal_date = models.DateTimeField(null=False)
-
   account_balance = MoneyField(max_digits=10, decimal_places=2, default_currency='USD')
+  
   def is_active(self):
-    if  (timezone.now - renewal_date) < 30:
+    if  (datetime.datetime.now - datetime.datetime(renewal_date)) < 30*3600:
       return True     
   def is_premium(self):
     curr = account_balance
@@ -19,6 +20,8 @@ class Account(models.Model):
     if curr < 20:
       return False
     return True  
+  
+  
 #should create a profile model that has a one to many relationship with accounts model and 
 #one to one relationship with the User model
   
