59564c64d01bed95404dfefc1771bec91cacd5cd
myk4040okothogodo <65109328+myk4040okothogodo@users.noreply.github.com>
2022-01-10 12:02:46 +0300

    Update serializers.py

--- whippAPI/user/serializers.py
+++ whippAPI/user/serializers.py
@@ -2,11 +2,18 @@ from whippAPI.user.models import User
 from rest_framework import serializers
 
 
+
 class UserSerializer(serializers.ModelSerializer):
+  
+  links = serializers.SerializerMethodField('get_links')
   class Meta:
     model = User
     fields = ['id', 'username','email', 'is_active', 'date_joined']
     read_only_field = ['is_active', 'date_joined']
+    
+  def get_links(self, obj):
+    request = self.context['request']
+    
   
   def update(self, instance, validated_data):
     """ Performs an update on a User."""
